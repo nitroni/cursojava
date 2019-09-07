510b73a3ddac311b573a6d8440632ccdf2890262
s109e8 <nitroni45@gmail.com>
2019-09-07 11:25:56 -0500

    Se agrego la clase Seguimiento2 en la cual se realizo la actividad propuesta en clase

--- src/seguimiento2/Seguimiento2.java
+++ src/seguimiento2/Seguimiento2.java
@@ -30,14 +30,17 @@ public class Seguimiento2 {
     
     public static void numeros1Al100(){
         
-         System.out.println("Numeros incrementales del 100 al 1");
+         System.out.println("----1.Numeros incrementales del 100 al 1----");
         
         //Impresion de datos ciclo For del 1 al 100
+        System.out.println("---Ciclo For---");
         for(int i=1; i <=100; i++){
             System.out.println(i);
         }
         
         //Impresion de datos ciclo while del 1 al 100
+        
+        System.out.println("---Ciclo while---");
         int i=1;
         while(i <=100){
             
@@ -48,14 +51,16 @@ public class Seguimiento2 {
     
     public static void numeros100Al1(){
         
-        System.out.println("Numeros incrementales del 100 al 1");
+        System.out.println("----2.Numeros incrementales del 100 al 1----");
         
         //Impresion de datos ciclo For del 100 al 1
+        System.out.println("---Ciclo For---");
         for(int i = 100; i >= 1; i--){
             System.out.println(i);
         }
         
         //Impresion de datos ciclo while del 100 al 1
+        System.out.println("---Ciclo while---");
         int i = 100;
         while(i >= 1 ){
             
@@ -67,9 +72,10 @@ public class Seguimiento2 {
     
     public static void numerospares1Al150(){
         
-        System.out.println("Numeros pares del 1 al 150");
+        System.out.println("---3.Numeros pares del 1 al 150---");
         
         //Impresion de datos ciclo For del 1 al 150
+        System.out.println("---Ciclo For---");
         for(int i = 1; i <= 150; i++){
             
             if((i%2)==0){
@@ -79,6 +85,7 @@ public class Seguimiento2 {
         }
         
         //Impresion de datos ciclo while del 1 al 150
+        System.out.println("---Ciclo while---");
         int i = 1;
         while(i <= 150){
             
@@ -94,11 +101,12 @@ public class Seguimiento2 {
     
     public static void sumaNumeros1Al200(){
         
-        System.out.println("Suma de los número del 1 al 200");
+        System.out.println("---4.Suma de los número del 1 al 200----");
         
         double suma=0;
         
         //Impresion de datos ciclo For de la suma del 1 al 200
+        System.out.println("---Ciclo for---");
         for(int i=1; i<=200; i++){
             suma=suma+i;
         }
@@ -106,6 +114,7 @@ public class Seguimiento2 {
         
         
         //Impresion de datos ciclo while de la suma del 1 al 200
+        System.out.println("---Ciclo while---");
         suma= 0;
         int i = 0;
         while(i <= 200){
@@ -121,20 +130,21 @@ public class Seguimiento2 {
         
         Scanner scanner = new Scanner(System.in);
         
-        System.out.println("Mostrar asteriscos");
+        System.out.println("---5. Mostrar asteriscos----");
         
         System.out.print("Ingrese el número: ");
         int numero = scanner.nextInt();
         
         //Impresion de datos ciclo For del número de asteriscos
+        System.out.println("---Ciclo For---");
         for(int i=1; i <= numero; i++){
             System.out.print("* ");
         }
         
         
         //Impresion de datos ciclo While del número de asteriscos
-        System.out.println("Ciclo while");
-        int i=0;
+        System.out.println("---Ciclo while---");
+        int i=1;
         while(i <= numero){
             System.out.print("* ");
             i++;
@@ -145,14 +155,15 @@ public class Seguimiento2 {
     public static void imprimirTabla10Por10(){
         
         
-           System.out.println("-------Cilo For Mostrar matriz de 10 * 10, en donde X representa los Pares y O los impares-----");
+           System.out.println("-------6. Cilo For Mostrar matriz de 10 * 10, en donde X representa los Pares y O los impares-----");
         
            String [][] matriz = new String[10][10];
            
            int contador = 0;
            
             
-           //Impresion de datos ciclo for matriz 10 * 10 
+           //Impresion de datos ciclo for matriz 10 * 10
+           System.out.println("---Ciclo For---");
            for(int i=0; i< 10; i++){
                
                for(int j = 0; j< 9; j++){
@@ -176,7 +187,7 @@ public class Seguimiento2 {
                
             }
            
-            System.out.println("------Ciclo while Mostrar matriz de 10 * 10, en donde X representa los Pares y O los impares ---");
+            System.out.println("---Ciclo while---");
             //Impresion de datos ciclo While matriz 10 * 10
             int i =0;
             int j =0;
