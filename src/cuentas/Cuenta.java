3a1a8d1fdf9f33ef5b938dc1a9be745782a2923a
Henry <>
2019-09-19 22:09:20 -0500

    Se adiciona el programa de cuenta

--- src/cuentas/Cuenta.java
+++ src/cuentas/Cuenta.java
@@ -50,7 +50,7 @@ public class Cuenta {
     public void retirar(double cantidad){
         
         if(cantidad > this.getSaldo()){
-            System.out.println("SALDO INSUFICIENTE");
+            System.out.println("SALDO INSUFICIENTE.");
         }
         else{
             this.setSaldo((this.getSaldo()-cantidad));
