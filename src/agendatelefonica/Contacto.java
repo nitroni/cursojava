bfc51c566013c42f6cbc99268882f095aeafa3f2
Henry <>
2019-09-19 22:48:51 -0500

    Se adiciona el programa de agenda teléfonica

---
+++ src/agendatelefonica/Contacto.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agendatelefonica;
+
+/**
+ *
+ * @author Henry A.
+ */
+public class Contacto {
+    
+    private String nombre;
+    private int telefono;
+    private String correo;
+    
+    public Contacto(String nombre, int telefono, String correo){
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    @Override
+    public String toString() {
+        return "nombre=" + nombre + ", telefono=" + telefono+ ", correo=" + correo;
+    }
+     
+    
+}
