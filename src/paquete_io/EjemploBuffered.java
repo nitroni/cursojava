5841d9ab4a6f6f6d20cc5a9ff30735fadc78938f
s109e8 <s109e8@B14_109_08>
2019-10-19 11:57:31 -0500

    se adiciona la clase ejemploBuffered

---
+++ src/paquete_io/EjemploBuffered.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete_io;
+
+import java.io.IOException;
+/**
+ *
+ * @author s109e8
+ */
+public class EjemploBuffered {
+    
+
+
+private static void crearArchivoBuffered() throws IOException{
+
+
+    
+    
+}
+
+private static void leerArchivo() throws IOException{
+
+
+    
+    
+}
+
+
+
+}
